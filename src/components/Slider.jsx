8ce4d0905de891a5b8b34fdace963676b1236917
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-02-15 17:25:30 +0600

    saved contexts

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import Overlay from "./Overlay";
-
+import Overlay from "../components/Overlay";
 
 const slides = [
   "https://images.pexels.com/photos/965989/pexels-photo-965989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
